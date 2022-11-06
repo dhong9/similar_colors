02c14a86d8ac1f980ef2cd4485c2ed71227ff697
Daniel Hong <danielhong35@gmail.com>
2022-11-06 10:34:34 -0800

    Added kNN children back in

--- src/components/knn/knn.component.js
+++ src/components/knn/knn.component.js
@@ -5,11 +5,16 @@ import Addpoint from "../addpoint/addpoint.component";
 import ScatterPlot from "../plot/plot.component";
 import Table from "../table/table.component";
 
+// Data
+import data from "../../data/points.csv";
+
 export default function KNN() {
 
     return (
         <div>
-            KNN Component
+            <Table />
+            <ScatterPlot />
+            <Addpoint />
         </div>
     );
 
