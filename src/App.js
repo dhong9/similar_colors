2a561b46d3019350935efa98e9dd2fb9db096ac7
Daniel Hong <danielhong35@gmail.com>
2022-11-06 10:32:36 -0800

    Added kNN component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 
 // Components
-import Table from './components/table/table.component';
-import ScatterPlot from './components/plot/plot.component';
-import Addpoint from './components/addpoint/addpoint.component';
+import KNN from './components/knn/knn.component';
 
 function App() {
   return (
     <div>
       <h1>kNN Data</h1>
-      <Table />
-      <ScatterPlot />
-      <Addpoint />
+      <KNN />
     </div>
   );
 }
---
+++ src/components/knn/knn.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Components
+import Addpoint from "../addpoint/addpoint.component";
+import ScatterPlot from "../plot/plot.component";
+import Table from "../table/table.component";
+
+export default function KNN() {
+
+    return (
+        <div>
+            KNN Component
+        </div>
+    );
+
+}
