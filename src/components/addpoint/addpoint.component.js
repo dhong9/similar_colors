4076e68a6762364ebf3e504eaa74e4bfeb54b3e7
Daniel Hong <danielhong35@gmail.com>
2022-11-06 10:26:09 -0800

    Read input points

--- src/components/addpoint/addpoint.component.js
+++ src/components/addpoint/addpoint.component.js
@@ -2,16 +2,41 @@ import React, { useState } from "react";
 
 export default function Addpoint() {
 
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = event =>
+        setInputs(values => ({
+            ...values,
+            [event.target.name] : event.target.value
+        }));
+    
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        //! TODO Show input points on plot
+        console.log(inputs);
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>x
-                <input type="number" />
+                <input
+                    type="number"
+                    name="x"
+                    value={inputs.x ?? ""}
+                    onChange={handleChange}
+                />
             </label>
             <label>y
-                <input type="number" />
+                <input
+                    type="number"
+                    name="y"
+                    value={inputs.y ?? ""}
+                    onChange={handleChange}
+                />
             </label>
             <input type="submit" />
         </form>
-    )
+    );
 
 }
