801bb32915e1e27800fe3d04b8cd2d8a87152ecf
Daniel Hong <danielhong35@gmail.com>
2022-11-06 15:37:10 -0800

    Added comments to Addpoint component

--- src/components/addpoint/addpoint.component.js
+++ src/components/addpoint/addpoint.component.js
@@ -1,25 +1,41 @@
+// React
 import React, { useState } from "react";
 
 // CSS
 import "./addpoint.component.css";
 
+/**
+ * Form for adding input point and number of neighbors
+ * @param {Object} props contanis form submission function
+ * @returns form component
+ */
 export default function Addpoint(props) {
-
+    
+    // Getting and setting form data
     const [inputs, setInputs] = useState({});
 
+    /**
+     * Updates input data from user
+     * @param {Event} event when user enters data
+     */
     const handleChange = event =>
         setInputs(values => ({
             ...values,
             [event.target.name] : event.target.value
         }));
     
+    /**
+     * Handles form submission event
+     * @param {Event} event when user submits form
+     */
     const handleSubmit = event => {
         event.preventDefault();
-        props.onSubmit(inputs);
+        props.onSubmit(inputs); // Update parent with user input
     };
 
     return (
         <form onSubmit={handleSubmit}>
+            {/* Data point */}
             <label>x
                 <input
                     className="input"
@@ -38,6 +54,8 @@ export default function Addpoint(props) {
                     onChange={handleChange}
                 />
             </label>
+
+            {/* Number of neighbors */}
             <label>k
                 <input
                     className="input"
@@ -47,6 +65,8 @@ export default function Addpoint(props) {
                     onChange={handleChange}
                 />
             </label>
+
+            {/* Form submission button */}
             <input className="button" type="submit" />
         </form>
     );
