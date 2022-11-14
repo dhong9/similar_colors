ff823d494a4f126c1499ad1c447f6adac04e87f8
Daniel Hong <danielhong35@gmail.com>
2022-11-13 19:23:46 -0800

    Sample home page test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders kNN Data', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/kNN Data/i);
   expect(linkElement).toBeInTheDocument();
 });
