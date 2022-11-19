d3431460985844b382111e32a77f10864e0316ae
Daniel Hong <danielhong35@gmail.com>
2022-11-18 21:30:03 -0800

    Switching to React testing recipes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
 import App from './App';
 
-test('renders kNN Data', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/kNN Data/i);
-  expect(linkElement).toBeInTheDocument();
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders title", () => {
+  act(() => {
+    render(<App />, container);
+  });
+  expect(container.textContent).toBe("kNN Data");
 });
